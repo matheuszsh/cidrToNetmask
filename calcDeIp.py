66d5a3e3526ab12f4b8e03fcebcaed28c081e871
matheuszsh <cyberestudante@gmail.com>
2025-03-03 09:41:17 -0300

    update: complete list cidr 32

--- calcDeIp.py
+++ calcDeIp.py
@@ -4,7 +4,7 @@ def main():
 
     while loopStop == True:
         try:
-            CIDRLIST = [8,16,24,25,27,28,29,30,31,32]
+            CIDRLIST = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
 
             print(f"Mascaras de rede disponível: {CIDRLIST}")
 
